f81a8260639fa379453e36d58df0a256698ae387
Mohammed Zubair A <mdzub7@gmail.com>
2025-04-30 12:58:41 +0530

    MAC basic v1

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from 'next/font/google'
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
 import { AuthProvider } from "@/context/AuthProvider";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata = {
   title: "Xen-AI ",
@@ -19,16 +15,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <AuthProvider>   
-          <Provider>
-          {children}
-          </Provider>
+    <html lang="en" className={inter.className}>
+      <body>
+        <AuthProvider>
+          <Provider>{children}</Provider>
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
